fa4b0a3cfa3baafbb2d51de451aa8b614181ecd2
Dat Truong <21522922@gm.uit.edu.vn>
2023-12-23 11:32:23 +0700

    test8

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const home = require('./routes/home')
+const routers = require('./routers/index')
 
 const app = express()
 
 app.use('/app', home);
+app.use('/notapp', routers);
 
 app.listen(4567, () => console.log("connected"))
